2fe12c7b4dc71e54881ee0a9810d4ee8dae43b8a
yhd1101 <asd159369@naver.com>
2023-05-24 17:12:49 +0900

    review01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [keyword, setKeyword] = useState("") //문자열으로 바꾸어줌 데이터 담는 그릇
+
+  const changeKeyword = () => {
+    console.log("키워드입니다.")
+      setKeyword("키워드")
+  }
+    return (
+        <div>
+            <h1>{keyword}</h1>
+                <button onClick={changeKeyword}>키워드 번경</button>
+        </div>
+    );
+};
 
 export default App;
